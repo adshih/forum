5c5e6e2253ac6f4189f11038c3ad57ac83d1ae10
Adam <shihadam1@gmail.com>
2023-03-01 20:42:53 -0800

    Altered types

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,8 @@
 create table if not exists users (
     id          uuid primary key default gen_random_uuid(),
-    username    varchar(50) unique not null,
+    username    varchar(20) unique not null,
+    email       varchar(254) unique not null,
     password    text not null,
     score       integer default 0,
-    created_at  timestamptz default now()
+    created_at  timestamptz not null default now()
 );
